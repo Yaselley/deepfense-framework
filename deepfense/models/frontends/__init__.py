626d3175006cd83c31a007f627027d38ec5291ab
Yaselley <elkheiryassine0@gmail.com>
2025-11-17 12:26:29 +0100

    update registers

--- deepfense/models/frontends/__init__.py
+++ deepfense/models/frontends/__init__.py
@@ -1,2 +1,4 @@
 from deepfense.models.frontends.registry import FRONTEND
 from deepfense.models.frontends.Wav2Vec2 import *
+from deepfense.models.frontends.WavLM import *
+from deepfense.models.frontends.HuBERT import *
